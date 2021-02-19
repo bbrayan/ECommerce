78b66f9d24a80965b25c9f3a368fd2b912fbb748
Brayan Boukhman <brayanboukhman@yahoo.ca>
2021-02-18 23:21:19 -0500

    small rpoblem

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name: 'cool shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock: 10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'less cool shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            countInStock: 20,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'third shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            countInStock: 0,
+            price:100,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'math is fun pants',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:120,
+            countInStock: 8,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'the last shirt',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:120,
+            countInStock: 10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'normal Pants',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:10,
+            countInStock: 230,
+            brand:'Nike',
+            rating:1.0,
+            numReviews:10,
+            description: 'high quality product'
+        }
+    ]
+}
+
+export default data;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import data from './data.js'
 
 const app = express();
 
+/* req is short for request and res is for responce */
+
+app.get('/api/products', (req,res) =>{
+    res.send(data.products);
+})
+
+app.get('/', (req, res) =>{
+    res.send('Server is ready');
+});
+const port = process.eventNames.PORT || 5000;
+app.listen(5000, ()=>{
+    console.log(`Serve at http://localhost:${port}`)
+})
