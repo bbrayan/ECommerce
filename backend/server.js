6f7ad1c3c1c46f25594fb8af3b925456db828690
Brayan Boukhman <brayanboukhman@yahoo.ca>
2021-02-19 23:55:22 -0500

    got  redux setup

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,18 @@ const app = express();
 
 /* req is short for request and res is for responce */
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
+  });
+
 app.get('/api/products', (req,res) =>{
     res.send(data.products);
-})
+});
 
 app.get('/', (req, res) =>{
     res.send('Server is ready');
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,14 @@
 
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 /* redux-thunk make it possible to send ajax recuest in redux action  */
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
